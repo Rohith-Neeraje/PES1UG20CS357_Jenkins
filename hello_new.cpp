d940461d51c5c01e1a58dc417a172c38be2c8038
Rohith-Neeraje <115130014+Rohith-Neeraje@users.noreply.github.com>
2023-02-18 21:38:18 +0530

    Create hello_new.cpp

---
+++ hello_new.cpp
@@ -0,0 +1,10 @@
+//File: hello_new.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I am Rohith Neeraje,PES1UG20CS357\n";
+  return 0;
+}
